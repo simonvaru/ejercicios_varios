da503a650d252d4f08f1815bc9aff9c6d57e239f
simonvaru <pennywise1540b@hotmail.com>
2022-09-17 20:02:09 -0300

    tree more fancier and nicer

--- draw_forest_random_trees_objects .py
+++ draw_forest_random_trees_objects .py
@@ -48,46 +48,72 @@ import numpy as np
 import cv2 as cv
 import random
 
-#general parameters
+######################
+##general parameters##
+######################
 width = 900
 height = 600
 n_tress = 30
 ground_level = height -100
 
-#colours
-green, light_gree, brown = (40, 185, 40), (25, 220, 0), (30,65, 155)
+###########
+##colours##
+###########
+green, light_green, brown = (40, 185, 40), (25, 220, 0), (30,65, 155)
 
-#blank image
+###############
+##blank image##
+###############
 bg = np.zeros((height, width, 3), dtype = np.uint8)
 
-#draw background
+############
+##draw sky##
+############
 cv.rectangle(bg, (width, 0), (0, ground_level), (225, 225, 95), -1)
-cv.rectangle(bg, (width, ground_level), (0, height), green, -1)
+
 ################
 class Tree(object):
     def __init__(self, image, location):
         self.img = image
         self.loc =location
         self.ht = 300
-        self.radius = 100
+        self.radius = 50
+        self.scale = 2
     def draw(self):
-        #trunk=> cv.line(image, start_point, end_point, color, thickness) 
-        cv.line(self.img, (self.loc, ground_level), (self.loc, ground_level-self.ht), brown, 40)
-        cv.line(self.img, (self.loc, ground_level-self.ht + 150), (self.loc-90, ground_level-self.ht + self.radius-40), brown, 10)
-        cv.line(self.img, (self.loc, ground_level-self.ht + 150), (self.loc+90, ground_level-self.ht + self.radius-40), brown, 10)
+        small_radius = self.radius*self.scale-20*self.scale
         #leafs=> cv.circle(image, center_coordinates, radius, color, thickness)
-        cv.circle(self.img, (self.loc, ground_level-self.ht), self.radius, green, -1)
-        cv.circle(self.img, (self.loc-90, ground_level-self.ht + self.radius-40), self.radius-40, green, -1)
-        cv.circle(self.img, (self.loc+90, ground_level-self.ht + self.radius-40), self.radius-40, green, -1)
+        cv.circle(self.img, (self.loc, ground_level-self.ht), self.radius*self.scale, green, -1)
+        cv.circle(self.img, (self.loc-45*self.scale, ground_level-self.ht + small_radius), small_radius, green, -1)
+        cv.circle(self.img, (self.loc+45*self.scale, ground_level-self.ht + small_radius), small_radius, green, -1)
+        #trunk=> cv.line(image, start_point, end_point, color, thickness) 
+        cv.line(self.img, (self.loc, ground_level), (self.loc, ground_level-self.ht), brown, 20*self.scale)
+        cv.line(self.img, (self.loc, ground_level-self.ht + 75*self.scale), (self.loc-45*self.scale, ground_level-self.ht + small_radius), brown, 5*self.scale)
+        cv.line(self.img, (self.loc, ground_level-self.ht + 75*self.scale), (self.loc+45*self.scale, ground_level-self.ht + small_radius), brown, 5*self.scale)
+        #leafs-shadows
+        cv.circle(self.img, (self.loc, ground_level-self.ht), self.radius*self.scale-10*self.scale, light_green, -1)
+        cv.circle(self.img, (self.loc-45*self.scale, ground_level-self.ht + small_radius), small_radius-10*self.scale, light_green, -1)
+        cv.circle(self.img, (self.loc+45*self.scale, ground_level-self.ht + small_radius), small_radius-10*self.scale, light_green, -1)
         return self.img
-        
     
-################
+    
+    
+################        
+    
 
-#DISPLAY IMAGE
+#################
+##display image##
+#################
 img = Tree(bg, 450).draw()
-cv.imshow('forest of objects', bg)
 
+###############
+##draw ground##
+###############
+cv.rectangle(bg, (width, ground_level), (0, height), green, -1)
+
+##############################
+##show in individual windows##
+##############################
+cv.imshow('forest of objects', bg)
 cv.waitKey(0)
 cv.destroyAllWindows()
 
